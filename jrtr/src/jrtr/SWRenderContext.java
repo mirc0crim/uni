74ad971140ed88705f81aafbb7c21f3562cc4180
Mirco Kocher <mirco.kocher@gmx.ch>
2011-10-26 23:12:54 +0200

    edge function && bounding box

--- jrtr/src/jrtr/SWRenderContext.java
+++ jrtr/src/jrtr/SWRenderContext.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
+import javax.vecmath.Matrix3f;
 import javax.vecmath.Matrix4f;
 import javax.vecmath.Vector4f;
 
@@ -74,8 +75,7 @@ public class SWRenderContext implements RenderContext {
 		viewMat.setM03((width - 1) / 2);
 		viewMat.setM13((height - 1) / 2);
 		viewMat.setM23(1 / 2);
-		colorBuffer = new BufferedImage(width, height,
-				BufferedImage.TYPE_3BYTE_BGR);
+		colorBuffer = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
 	}
 
 	/**
@@ -92,18 +92,85 @@ public class SWRenderContext implements RenderContext {
 	 * objects.
 	 */
 	private void draw(RenderItem renderItem) {
+		VertexData vertDat = renderItem.getShape().getVertexData();
 		edges = new ArrayList<Vector4f>();
 		zBuffer = new int[vWidth][vHeight];
 		for (int i = 0; i < vHeight; i++)
 			for (int j = 0; j < vWidth; j++)
 				zBuffer[i][j] = Integer.MAX_VALUE;
 
-		projection(renderItem.getShape().getVertexData());
-		raster();
+		projection(vertDat);
+		raster(vertDat);
 	}
 
-	private void raster() {
+	private void raster(VertexData dat) {
+		for (int i = 0; i < edges.size(); i++) {
+			Vector4f a = edges.get(i++);
+			Vector4f b = edges.get(i++);
+			Vector4f c = edges.get(i++);
+
+			// edge function
+			Matrix3f edge = new Matrix3f();
+			boolean invertable = true;
+			edge.setM00(a.getX());
+			edge.setM01(a.getY());
+			edge.setM02(a.getW());
+			edge.setM10(b.getX());
+			edge.setM11(b.getY());
+			edge.setM12(b.getW());
+			edge.setM20(c.getX());
+			edge.setM21(c.getY());
+			edge.setM22(c.getW());
+			try {
+				edge.invert();
+			} catch (Exception e) {
+				invertable = false;
+			}
+
+			if (invertable)
+				if (pos(a) && pos(b) && pos(c)){
+					// do bounding box
+					int leftx = min(a.getX(), b.getX(), c.getX());
+					int rightx = max(a.getX(), b.getX(), c.getX());
+					int bottomy = min(a.getY(), b.getY(), c.getY());
+					int topy = max(a.getY(), b.getY(), c.getY());
+					drawInBox(edge, leftx, bottomy, rightx, topy, a, b, c);
+				} else
+					drawInBox(edge, 0, 0 , vWidth, vHeight, a, b, c);
+
+		}
 	}
+
+
+	private void drawInBox(Matrix3f edge, int leftx, int bottomy, int rightx, int topy,
+			Vector4f a, Vector4f b, Vector4f c) {
+		for (int i = leftx; i < rightx; i++)
+			for (int j = topy; j > bottomy; j--) {
+				// alpha beta gamma functions
+			}
+
+	}
+
+	private boolean pos(Vector4f a) {
+		return a.getX() > 0 && a.getY() > 0;
+	}
+
+	private int min(float a, float b, float c) {
+		if (a < b && a < c)
+			return (int) a;
+		if (b < a && b < c)
+			return (int) b;
+		return (int) c;
+	}
+
+	private int max(float a, float b, float c) {
+		if (a > b && a > c)
+			return (int) a;
+		if (b > a && b > c)
+			return (int) b;
+		return (int) c;
+	}
+
 
 	private void projection(VertexData vertexData) {
 		LinkedList<VertexElement> vertexEle = vertexData.getElements();
@@ -116,8 +183,7 @@ public class SWRenderContext implements RenderContext {
 		temp.mul(Camera.getCameraMatrix());
 		Vector4f vec;
 		for (int k : index) {
-			ListIterator<VertexData.VertexElement> itr = vertexEle
-					.listIterator(0);
+			ListIterator<VertexData.VertexElement> itr = vertexEle.listIterator(0);
 			while (itr.hasNext()) {
 				VertexData.VertexElement e = itr.next();
 				if (e.getSemantic() == VertexData.Semantic.POSITION) {
@@ -127,13 +193,14 @@ public class SWRenderContext implements RenderContext {
 					vec = new Vector4f(x, y, z, 1);
 					temp.transform(vec);
 
+					edges.add(vec);
+
 					vec.setX(vec.getX() / vec.getW());
 					vec.setY(vec.getY() / vec.getW());
 					if (vec.getX() > 0 && vec.getY() > 0 && vec.getX() < vWidth
 							&& vec.getY() < vHeight)
-						colorBuffer.setRGB((int) vec.getX(), vHeight
-								- (int) vec.getY(), Color.white.getRGB());
-					edges.add(vec);
+						colorBuffer.setRGB((int) vec.getX(), vHeight - (int) vec.getY(),
+								Color.white.getRGB());
 				}
 			}
 		}
