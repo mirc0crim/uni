587f9da1e49f296c04d98ecc11c7c9bbd1fd6cfc
Mirco Kocher <mirco.kocher@gmx.ch>
2011-10-31 00:58:50 +0100

    ki-2-2: some styling, made it MOAR NICE!!! :P

--- octave/zzwei.m
+++ octave/zzwei.m
@@ -1,6 +1,10 @@
-function fn = zzwei(x1,y1,z1,b1,x2,y2,z2,b2,x3,y3,z3,b3)
-	A = [x1,y1,z1,b1;x2,y2,z2,b2;x3,y3,z3,b3]
+function zzwei(x1,y1,z1,b1,x2,y2,z2,b2,x3,y3,z3,b3)
+	A = [x1,y1,z1,b1;x2,y2,z2,b2;x3,y3,z3,b3];
+	fprintf('\n  %+.1fx %+.1fy %+.1fz = %+.1f\n', A(1,1), A(1,2), A(1,3), A(1,4))
+	fprintf('  %+.1fx %+.1fy %+.1fz = %+.1f\n', A(2,1), A(2,2), A(2,3), A(2,4))
+	fprintf('  %+.1fx %+.1fy %+.1fz = %+.1f\n\n', A(3,1), A(3,2), A(3,3), A(3,4))
 	unique = 1;
+
 	### calculations ###
 	if (A(1,1) != 0)
 		A = [1,A(1,2)/A(1,1),A(1,3)/A(1,1),A(1,4)/A(1,1);A(2,1),A(2,2),A(2,3),A(2,4);A(3,1),A(3,2),A(3,3),A(3,4)];
@@ -24,44 +28,43 @@ function fn = zzwei(x1,y1,z1,b1,x2,y2,z2,b2,x3,y3,z3,b3)
 	# A(2,3) = 0
 	A = [A(1,1),A(1,2),A(1,3),A(1,4);A(2,1)-A(3,1)*A(2,3),A(2,2)-A(3,2)*A(2,3),A(2,3)-A(2,3)*A(3,3),A(2,4)-A(2,3)*A(3,4);A(3,1),A(3,2),A(3,3),A(3,4)];
 	
-	A = [A(1,1),A(1,2),A(1,3),A(1,4);A(2,1),A(2,2),A(2,3),A(2,4);A(3,1),A(3,2),A(3,3),A(3,4)]
+	A = [A(1,1),A(1,2),A(1,3),A(1,4);A(2,1),A(2,2),A(2,3),A(2,4);A(3,1),A(3,2),A(3,3),A(3,4)];
 
-	fprintf('----------------------\n')
+	fprintf('-------------------------\n  Solution:\n')
 
 	### special cases ###
-	# not unique
-	if(A(1,1) == 0)
-		fprintf('  %.2fy= %f %+fx \n', A(2,2), A(2,4), -A(2,1))
-		fprintf('  %.2fz= %f %+fx \n\n', A(3,3), A(3,4), -A(3,1))
-		unique = 0;
-	endif
-	if(A(2,2) == 0)
-		fprintf('  %.2fx= %.2f %+.2fy \n', A(1,1), A(1,4), -A(1,2))
-		fprintf('  %.2fz= %.2f %+.2fy \n\n', A(3,3), A(3,4), -A(3,2))
-		unique = 0;
-	endif
-	if(A(3,3) == 0)
-		fprintf('  %.2fx= %.2f %+.2fz \n', A(1,1), A(1,4), -A(1,3))
-		fprintf('  %.2fy= %.2f %+.2fz \n\n', A(2,2), A(2,4), -A(2,3))
-		unique = 0;
-	endif
-
 	# zeroes
 	if (A(1,1:3) == 0 && A(1,4) != 0)
-		fprintf('This equation is not solvable because 0 = %d \n\n', A(1,4))
+		fprintf('  This equation is not solvable because 0 = %d \n\n', A(1,4))
 		unique = 0;
 	endif
 	if (A(2,1:3) == 0 && A(2,4) != 0)
-		fprintf('This equation is not solvable because 0 = %d \n\n', A(2,4))
+		fprintf('  This equation is not solvable because 0 = %d \n\n', A(2,4))
 		unique = 0;
 	endif
 	if (A(3,1:3) == 0 && A(3,4) != 0)
-		fprintf('This equation is not solvable because 0 = %d \n\n', A(3,4))
+		fprintf('  This equation is not solvable because 0 = %d \n\n', A(3,4))
+		unique = 0;
+	endif
+	# not unique
+	if(A(1,1) == 0 && unique == 1)
+		fprintf('  %+.2fy= %+.2f %+.2fx \n', A(2,2), A(2,4), -A(2,1))
+		fprintf('  %+.2fz= %+.2f %+.2fx \n\n', A(3,3), A(3,4), -A(3,1))
+		unique = 0;
+	endif
+	if(A(2,2) == 0 && unique == 1)
+		fprintf('  %+.2fx= %+.2f %+.2fy \n', A(1,1), A(1,4), -A(1,2))
+		fprintf('  %+.2fz= %+.2f %+.2fy \n\n', A(3,3), A(3,4), -A(3,2))
+		unique = 0;
+	endif
+	if(A(3,3) == 0 && unique == 1)
+		fprintf('  %+.2fx= %+.2f %+.2fz \n', A(1,1), A(1,4), -A(1,3))
+		fprintf('  %+.2fy= %+.2f %+.2fz \n\n', A(2,2), A(2,4), -A(2,3))
 		unique = 0;
 	endif
 
 	### result ###
 	if (unique == 1)
-		fn = [A(1,4),A(2,4),A(3,4)];
+		fprintf('  x=%+.1f\n  y=%+.1f\n  z=%+.1f \n\n', A(1,4), A(2,4), A(3,4))
 	endif
 endfunction
