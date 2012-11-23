108509f3faf6e0b5c9c25dfe57359dc24c47e69d
Mirco Kocher <mirco.kocher@gmx.ch>
2012-11-23 16:15:10 +0100

    -*-MoCo-*- google more info on pause pin

--- Yala/src/ch/unibe/yala/MyItemOverlay.java
+++ Yala/src/ch/unibe/yala/MyItemOverlay.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
@@ -49,8 +53,20 @@ public class MyItemOverlay extends ItemizedOverlay<OverlayItem> {
 		OverlayItem item = mOverlays.get(index);
 		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
 		dialog.setTitle(item.getTitle());
-		if (item.getSnippet().toString().length() > 0)
-			dialog.setMessage(item.getSnippet());
+		final String snippet = item.getSnippet();
+		if (snippet.toString().length() > 0) {
+			dialog.setMessage(snippet);
+			dialog.setPositiveButton("More Info", new OnClickListener() {
+				@Override
+				public void onClick(DialogInterface arg0, int arg1) {
+					String[] snippetArray = snippet.replace(" ", "+").split("\n");
+					Uri uri = Uri.parse("https://www.google.com/search?q=" + snippetArray[0] + "+"
+							+ snippetArray[1]);
+					Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+					mContext.startActivity(intent);
+				}
+			});
+		}
 		dialog.show();
 		return true;
 	}
