e8a4321a8c9b2babbadfec50a56480ff49220c61
Mirco Kocher <mirco.kocher@gmx.ch>
2013-02-26 12:10:27 +0100

    -*-Ray-*- intersectablelist

--- ray tracer/src/Main.java
+++ ray tracer/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.vecmath.Matrix4f;
 import javax.vecmath.Vector3f;
 import javax.vecmath.Vector4f;
@@ -6,8 +9,11 @@ public class Main {
 
 	public static Camera camera;
 	public static Film film;
+	public static List<Intersectable> IntersectableList;
 
 	public static void main(String[] args) {
+
+		IntersectableList = new ArrayList<Intersectable>();
 
 		Vector3f eye = new Vector3f(0, 0, 2);
 		Vector3f lookAt = new Vector3f(0, 0, 0);
@@ -21,15 +27,18 @@ public class Main {
 		Vector3f normal = new Vector3f(0, 1, 0);
 		float d = 1;
 		Plane plane = new Plane(normal, d);
+		IntersectableList.add(plane);
 
 		Vector3f v1 = new Vector3f(1, 0, 0);
 		Vector3f v2 = new Vector3f(1, 1, 0);
 		Vector3f v3 = new Vector3f(0, 1, 0);
 		Triangle triangle = new Triangle(v1, v2, v3);
+		IntersectableList.add(triangle);
 
 		Vector3f c = new Vector3f(0, 0, 0);
 		float r = 1.5f;
 		Sphere sphere = new Sphere(c, r);
+		IntersectableList.add(sphere);
 
 		film = new Film(width, height);
 
@@ -38,36 +47,32 @@ public class Main {
 				Vector4f primaryRay = new Vector4f(camera.getPixelRay(w, h));
 				primaryRay = Ray.transform(new Matrix4f(camera.getCameraMatrix()),
 						new Vector4f(camera.getPixelRay(w, h)));
-				float intersectionPlane = plane.testIntersection(new Vector3f(primaryRay.x,
-						primaryRay.y, primaryRay.z), new Vector3f(eye));
-				float intersectionTriangle = triangle.testIntersection(new Vector3f(
-						primaryRay.x, primaryRay.y, primaryRay.z), new Vector3f(eye));
-				float intersectionSphere = sphere.testIntersection(new Vector3f(primaryRay.x,
-						primaryRay.y, primaryRay.z), new Vector3f(eye));
+				float[] distList = new float[IntersectableList.size()];
+				int i = 0;
+				for (Intersectable object : IntersectableList) {
+					distList[i] = object.testIntersection(new Vector3f(primaryRay.x,
+							primaryRay.y, primaryRay.z), new Vector3f(eye));
+					i++;
+				}
 				int rgb = 0;
 				int red = 0;
 				int green = 0;
 				int blue = 0;
-				if (intersectionSphere < 0) {
-					// red = (int) (Math.abs(primaryRay.x) * 255);
-					if (intersectionSphere < -5) {
+				if (distList[2] < 0) {
+					if (distList[2] < -5) {
 						blue = 0;
 					} else
-						blue = 255 - (int) (intersectionSphere * -51);
-				} else if (intersectionTriangle < 0) {
-					// blue = (int) (Math.abs(primaryRay.x) * 255);
-					// red = (int) (Math.abs(primaryRay.y) * 255);
-					if (intersectionTriangle < -5)
+						blue = 255 - (int) (distList[2] * -51);
+				} else if (distList[1] < 0) {
+					if (distList[1] < -5)
 						green = 0;
 					else
-						green = 255 - (int) (intersectionTriangle * -51);
-				} else if (intersectionPlane < 0) {
-					// green = (int) (Math.abs(primaryRay.x) * 255);
-					// blue = (int) (Math.abs(primaryRay.y) * 255);
-					if (intersectionPlane < -5)
+						green = 255 - (int) (distList[1] * -51);
+				} else if (distList[0] < 0) {
+					if (distList[0] < -5)
 						red = 0;
 					else
-						red = 255 - (int) (intersectionPlane * -51);
+						red = 255 - (int) (distList[0] * -51);
 				}
 				rgb = (red << 16) + (green << 8) + blue;
 				film.setPixel(w, h, rgb);
