8345cfe40a2fef3220782f57982ec3a6e97287bb
Mirco Kocher <mirco.kocher@gmx.ch>
2013-03-22 20:12:08 +0100

    -*-ray-*- BSP init

--- ray tracer/src/rt/Aggregate.java
+++ ray tracer/src/rt/Aggregate.java
@@ -5,9 +5,18 @@ import java.util.ArrayList;
 public abstract class Aggregate {
 
 	protected ArrayList<Intersectable> intersectables;
+	protected Boundingbox box;
 
 	public Aggregate() {
 		intersectables = new ArrayList<Intersectable>();
+		ArrayList<Boundingbox> bb = new ArrayList<Boundingbox>();
+		for (Intersectable t : intersectables)
+			bb.add(t.getBox());
+		box = Boundingbox.combineBox(bb);
+	}
+
+	public Boundingbox getBox() {
+		return box;
 	}
 
 	public ArrayList<Intersectable> getIntersectables() {
