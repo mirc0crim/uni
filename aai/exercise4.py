52afe1428ddfd4b84d0d0697ff6878f667b299f4
Mirco Kocher <mirco.kocher@gmx.ch>
2013-04-09 11:27:11 +0200

    -*-aai-*- read file, extract num

--- aai/exercise4.py
+++ aai/exercise4.py
@@ -4,27 +4,38 @@ import random
 import re
 import scipy
 
-b = []
 xsize = 1000
 ysize = 500
-r = []
 
-f = open("0.dat")
-for line in f:
-    if line[0] != ".":
-        asdf = re.findall("[\s*](\d*)[\s*](\d*)", line)
-        if len(asdf) > 0:
-            r.append(asdf[0])
-f.close()
+def output(num):
+    b = []
+    r = []
+    m = open("apa\\apa\\apa00\\app0032.dat")
+    a = m.read()
+    inc = re.findall("\.INCLUDE apa/data/([\w/]*)", a)
+    inc = inc[0].replace("/","\\")
+    char = re.findall("\.SEGMENT CHARACTER .* \? (.*)", a)
+    sect = re.findall("\.SEGMENT CHARACTER (.*) \? .*", a)
+    sect = sect[1][:sect[1].find("-")]
+    f = open("apa\\data\\" + inc + ".dat")
+    a = f.read()
+    pe = re.findall("\.PEN_DOWN([.*\d*\n*\s*]*)\.PEN_UP", a)
+    asdf = re.findall("[\s*](\d{1,})[\s*](\d{1,})", pe[int(sect)])
+    for i in range(len(asdf)):
+        if len(asdf[i]) > 0:
+            r.append(asdf[i])
 
-for i in range(xsize):
-    a = []
-    for j in range(ysize):
-        a.append([0,0,0])
-    b.append(a)
+    for i in range(xsize):
+        a = []
+        for j in range(ysize):
+            a.append([0,0,0])
+        b.append(a)
 
-for i in range(len(r)):
-    b[int(r[i][0])][int(r[i][1])] = [255,255,255]
+    for i in range(len(r)):
+        b[int(r[i][0])][int(r[i][1])] = [255,255,255]
 
-Img2=Image.fromarray(numpy.uint8(b))
-Img2.save("tdst.png")
+    Img2=Image.fromarray(numpy.uint8(b))
+    Img2.save(char[num].replace("\"","") + ".png")
+
+for i in range(4):
+    output(i)
