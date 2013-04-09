22e541ef5a0f92d70b99f34c62daaa79d9c9e1b8
Mirco Kocher <mirco.kocher@gmx.ch>
2013-04-09 15:54:18 +0200

    -*-aai-*- calc dHash, compare

--- aai/exercise4.py
+++ aai/exercise4.py
@@ -3,27 +3,73 @@ import numpy
 import random
 import re
 import scipy
+from numpy import *
 
-xsize = 1000
-ysize = 500
+hash = []
+names = []
 
-m = open("apa\\apa\\apa00\\app0034.dat")
-a = m.read()
-inc = re.findall("\.INCLUDE apa/data/([\w/]*)", a)
-inc = inc[0].replace("/","\\")
-char = re.findall("\.SEGMENT CHARACTER .* \? (.*)", a)
-sect = re.findall("\.SEGMENT CHARACTER (.*) \? .*", a)
+inc = []
+char = []
+sect = []
 
-def output(num, inc, char, sect):
+def init(f):
+    m = open("D:\\aai\\apa\\apa\\" + f + ".dat")
+    global inc, char, sect
+    a = m.read()
+    inc = re.findall("\.INCLUDE apa/data/([\w/]*)", a)
+    inc = inc[0].replace("/","\\")
+    char = re.findall("\.SEGMENT CHARACTER .* \? (.*)", a)
+    sect = re.findall("\.SEGMENT CHARACTER (.*) \? .*", a)
+
+def output(num):
+    global char, sect, hash, inc
+    b = makeImage(num)
+
+    Img2=Image.fromarray(numpy.uint8(b))
+    name = inc[-2:] + "-" + str(num) + "-" + char[num].replace("\"","") + ".png"
+    Img2.save("D:\\aai\\apa\\output\\" + name)
+    Img2 = Img2.resize((8,9), Image.BILINEAR)
+    c = [k[0] for k in list(Img2.getdata())]
+    hash.append(calcDHash(c))
+    names.append(name)
+    print "saved", name
+
+def compareMe(num):
+    global char, sect, hash, inc
+    b = makeImage(num)
+
+    Img2=Image.fromarray(numpy.uint8(b))
+    Img2.save("D:\\aai\\apa\\output\\test.png")
+    Img2.show()
+    Img2 = Img2.resize((8,9), Image.BILINEAR)
+    c = [k[0] for k in list(Img2.getdata())]
+    thisHash = calcDHash(c)
+    diff = []
+    for i in range(len(hash)):
+        d = 0
+        for j in range(len(hash[i])):
+            if hash[i][j] <> thisHash[i]:
+                d += 1
+        diff.append(d)
+
+    print diff
+    print "Input", str(num), "matched", str(argmin(diff))
+    print "dHash Hamming distance", str(min(diff))
+    print "", names[num]
+    Img1 = Image.open("D:\\aai\\apa\\output\\" + names[num])
+    Img1.show()
+
+def makeImage(num):
     b = []
     r = []
+    global sect, inc
     if sect[num].find("-") != -1:
         sect1 = sect[num][:sect[num].find("-")]
         sect2 = sect[num][sect[num].find("-")+1:]
     else:
         sect1 = sect[num]
         sect2 = -1
-    f = open("apa\\data\\" + inc + ".dat")
+    f = open("D:\\aai\\apa\\data\\" + inc + ".dat")
     a = f.read()
     pe = re.findall("\.PEN_DOWN([.*\d*\n*\s*]*)\.PEN_UP", a)
     asdf1 = re.findall("[\s*](\d{1,})[\s*](\d{1,})", pe[int(sect1)])
@@ -49,10 +95,21 @@ def output(num, inc, char, sect):
 
     for i in range(len(r)):
         b[int(r[i][0]) - minX][int(r[i][1]) - minY] = [255,255,255]
+    
+    return b
 
-    Img2=Image.fromarray(numpy.uint8(b))
-    Img2.save(str(num) + "-" + char[num].replace("\"","") + ".png")
-    print "saved", str(num) + "-" + char[num].replace("\"","") + ".png"
+def calcDHash(w):
+    bits = []
+    for i in range(len(w)-1):
+        if w[i] < w[i+1]:
+            bits.append(1)
+        else:
+            bits.append(0)
+    return bits
 
+init("apa00\\app0032")
 for i in range(len(char)):
-    output(i, inc, char, sect)
+    output(i)
+
+init("apa00\\app0034")
+compareMe(1)
