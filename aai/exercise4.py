d091cc019c2283b7fc38843faac3e9117f7804fc
Mirco Kocher <mirco.kocher@gmx.ch>
2013-04-09 14:23:00 +0200

    -*-aai-*- smaller img output

--- aai/exercise4.py
+++ aai/exercise4.py
@@ -7,35 +7,52 @@ import scipy
 xsize = 1000
 ysize = 500
 
-def output(num):
+m = open("apa\\apa\\apa00\\app0034.dat")
+a = m.read()
+inc = re.findall("\.INCLUDE apa/data/([\w/]*)", a)
+inc = inc[0].replace("/","\\")
+char = re.findall("\.SEGMENT CHARACTER .* \? (.*)", a)
+sect = re.findall("\.SEGMENT CHARACTER (.*) \? .*", a)
+
+def output(num, inc, char, sect):
     b = []
     r = []
-    m = open("apa\\apa\\apa00\\app0032.dat")
-    a = m.read()
-    inc = re.findall("\.INCLUDE apa/data/([\w/]*)", a)
-    inc = inc[0].replace("/","\\")
-    char = re.findall("\.SEGMENT CHARACTER .* \? (.*)", a)
-    sect = re.findall("\.SEGMENT CHARACTER (.*) \? .*", a)
-    sect = sect[1][:sect[1].find("-")]
+    if sect[num].find("-") != -1:
+        sect1 = sect[num][:sect[num].find("-")]
+        sect2 = sect[num][sect[num].find("-")+1:]
+    else:
+        sect1 = sect[num]
+        sect2 = -1
     f = open("apa\\data\\" + inc + ".dat")
     a = f.read()
     pe = re.findall("\.PEN_DOWN([.*\d*\n*\s*]*)\.PEN_UP", a)
-    asdf = re.findall("[\s*](\d{1,})[\s*](\d{1,})", pe[int(sect)])
-    for i in range(len(asdf)):
-        if len(asdf[i]) > 0:
-            r.append(asdf[i])
+    asdf1 = re.findall("[\s*](\d{1,})[\s*](\d{1,})", pe[int(sect1)])
+    for i in range(len(asdf1)):
+        if len(asdf1[i]) > 0:
+            r.append(asdf1[i])
+    if sect2 != -1:
+        asdf2 = re.findall("[\s*](\d{1,})[\s*](\d{1,})", pe[int(sect2)])
+        for i in range(len(asdf2)):
+            if len(asdf2[i]) > 0:
+                r.append(asdf2[i])
 
-    for i in range(xsize):
+    maxX = max([int(k[0]) for k in r])
+    maxY = max([int(k[1]) for k in r])
+    minX = min([int(k[0]) for k in r])
+    minY = min([int(k[1]) for k in r])
+
+    for i in range(maxX-minX + 1):
         a = []
-        for j in range(ysize):
+        for j in range(maxY-minY + 1):
             a.append([0,0,0])
         b.append(a)
 
     for i in range(len(r)):
-        b[int(r[i][0])][int(r[i][1])] = [255,255,255]
+        b[int(r[i][0]) - minX][int(r[i][1]) - minY] = [255,255,255]
 
     Img2=Image.fromarray(numpy.uint8(b))
-    Img2.save(char[num].replace("\"","") + ".png")
+    Img2.save(str(num) + "-" + char[num].replace("\"","") + ".png")
+    print "saved", str(num) + "-" + char[num].replace("\"","") + ".png"
 
-for i in range(4):
-    output(i)
+for i in range(len(char)):
+    output(i, inc, char, sect)
