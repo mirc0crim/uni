0983afd131f2b5f369ca1041f9869ad3ee472476
Mirco Kocher <mirco.kocher@gmx.ch>
2013-04-14 18:12:30 +0200

    -*-nlp-*- some styling

--- nlp/src/tp6.py
+++ nlp/src/tp6.py
@@ -10,12 +10,12 @@ t = []
 t.append(fileMK.readFile(fedHamPath))
 
 print()
+print(" Path to Federalist Hamilton " + fedHamPath)
+print(" Path to Output " + outputPath)
+print()
 print(" ----------------------------------------------------------------")
 print(" 100 most Significant Two-Word Phrases (w/o stemming & Stopwords)")
 print(" ----------------------------------------------------------------")
-print()
-print(" Path to Federalist Hamilton " + fedHamPath)
-print(" Path to Output " + outputPath)
 print()
 
 lower = t[0].lower()
@@ -27,25 +27,39 @@ dict2words = fileMK.sortedDictFromWords(g)
 s = ""
 for i in range(100):
     s += str(dict2words[i][0]) + "\n"
-fileMK.writeTextToFile(s, outputPath + "task1")    
+fileMK.writeTextToFile(s, outputPath + "task1a")    
+
+significance = 2.576
 
 print()
 print(" ----------------------------------------------------------------")
-print(" T-Test w/ alpha 5% -> 1.65")
+print(" T-Test w/ alpha 0.5% ->", significance)
 print(" Total Two-Word-Phrases:", len(dict2words))
 print(" ----------------------------------------------------------------")
 print()
 
 dict1word = fileMK.unsortedDictFromWords(noStop.split(" "))
 
-for i in range(5):
-    print("Two-Word Phrase at rank",i+1 , dict2words[i][0])
-    words = dict2words[i][0].split(" ")
+tens = [k for k in dict2words if k[1] == 10]
+score = [0,0]
+colls = ["", ""]
+
+for i in range(len(tens)):
+    words = tens[i][0].split(" ")
     N = len(noStop.split(" "))
     pOne = dict1word[words[0]] / N
     pTwo = dict1word[words[1]] / N
     Mzero = pOne * pTwo
-    x = dict2words[i][1] / N
+    x = tens[i][1] / N
     s2 = x*(1-x)
     t = (x-Mzero) / (math.sqrt(s2/N))
-    print(t)
+    if t < significance:
+        score[0] += 1
+        colls[0] += tens[i][0] + ", "
+    else:
+        score[1] += 1
+        colls[1] += tens[i][0] + ", "
+
+s = str(score[0]) + " no collocation:\n" + colls[0]
+s += "\n" + str(score[1]) + " collocation:\n" + colls[1]
+fileMK.writeTextToFile(s, outputPath + "task1b")
