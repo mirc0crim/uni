e0fb86a198e14138d4bb3feec2c69afb18ec6049
Mirco Kocher <mirco.kocher@gmx.ch>
2013-04-14 17:33:26 +0200

    -*-nlp-*- tp6 t-test

--- nlp/src/tp6.py
+++ nlp/src/tp6.py
@@ -1,5 +1,6 @@
 #!C:/Python33/python.exe -u
 # -*- coding: ascii -*-
+import math
 import fileMK
 
 outputPath = "D:\\nlp\\output\\"
@@ -22,10 +23,29 @@ noPunct = fileMK.replaceSigns(lower)
 stemmed = fileMK.stemText(noPunct)
 noStop = fileMK.removeStopWords(stemmed, None)
 g = fileMK.extract2gram(noStop)
-sorted_dict = fileMK.sortedDictFromWords(g)
+dict2words = fileMK.sortedDictFromWords(g)
 s = ""
 for i in range(100):
-    s += str(sorted_dict[i][0]) + "\n"
-fileMK.writeTextToFile(s, outputPath + "task1")
+    s += str(dict2words[i][0]) + "\n"
+fileMK.writeTextToFile(s, outputPath + "task1")    
 
-# TODO: t-Test with 5% significance    
+print()
+print(" ----------------------------------------------------------------")
+print(" T-Test w/ alpha 5% -> 1.65")
+print(" Total Two-Word-Phrases:", len(dict2words))
+print(" ----------------------------------------------------------------")
+print()
+
+dict1word = fileMK.unsortedDictFromWords(noStop.split(" "))
+
+for i in range(5):
+    print("Two-Word Phrase at rank",i+1 , dict2words[i][0])
+    words = dict2words[i][0].split(" ")
+    N = len(noStop.split(" "))
+    pOne = dict1word[words[0]] / N
+    pTwo = dict1word[words[1]] / N
+    Mzero = pOne * pTwo
+    x = dict2words[i][1] / N
+    s2 = x*(1-x)
+    t = (x-Mzero) / (math.sqrt(s2/N))
+    print(t)
