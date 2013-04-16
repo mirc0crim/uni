3252823f73297f3e434766394262c3940cb70cc8
Mirco Kocher <mirco.kocher@gmx.ch>
2013-04-16 18:19:46 +0200

    -*-aai-*- load Img, min size, refactoring

--- aai/exercise4.py
+++ aai/exercise4.py
@@ -14,13 +14,14 @@ char = []
 sect = []
 corr = 0
 fals = 0
+resize = (8,8)
 
 outPath = "D:\\aai\\apa\\output\\"
 
 def init(f):
-    m = open(f)
     global inc, char, sect
-    a = m.read()
+    
+    a = open(f).read()
     inc = re.findall("\.INCLUDE apa/data/([\w/]*)", a)
     inc = inc[0].replace("/","\\")
     char = re.findall("\.SEGMENT CHARACTER .* \? (.*)", a)
@@ -28,27 +29,47 @@ def init(f):
 
 def output(num):
     global char, sect, hash, inc
-    b = makeImage(num)
 
-    Img2=Image.fromarray(numpy.uint8(b))
+    Img2=Image.fromarray(numpy.uint8(makeImage(num)))
     name = inc[-2:] + "-" + str(num) + "-" + char[num].replace("\"","")
     Img2.save(outPath + name + ".png")
-    Img2 = Img2.resize((10,10), Image.ANTIALIAS)
+    Img2 = Img2.resize(resize, Image.ANTIALIAS)
     c = [k[0] for k in list(Img2.getdata())]
     hash.append(calcDHash(c))
     names.append(name + ".png")
-    #print "saved", name
 
 def compareMe(num):
     global char, sect, hash, inc, corr, fals
-    b = makeImage(num)
 
-    Img2=Image.fromarray(numpy.uint8(b))
+    Img2=Image.fromarray(numpy.uint8(makeImage(num)))
     Img2.save(outPath + "test-" + char[num].replace("\"","") + ".png")
-    #Img2.show()
-    Img2 = Img2.resize((10,10), Image.ANTIALIAS)
+    Img2 = Img2.resize(resize, Image.ANTIALIAS)
     c = [k[0] for k in list(Img2.getdata())]
     thisHash = calcDHash(c)
+    diff = compHash(thisHash, hash)
+
+    n = char[num].replace("\"","")
+    outText = "test-" + n +".png matched " + names[argmin(diff)]
+    if (n == names[argmin(diff)][-5]):
+        outText += " correctly"
+        corr += 1
+    else:
+        outText += " falsely"
+        fals += 1
+    print outText, "w/ dist", str(min(diff))
+
+def imgCompare():
+    Img3 = Image.open("D:\\aai\\apa\\hand.png")
+    Img3 = Img3.resize(resize, Image.ANTIALIAS)
+    c = [k[0] for k in list(Img3.getdata())]
+    thisHash = calcDHash(c)
+    diff = compHash(thisHash, hash)
+
+    print diff
+    outText = "hand.png matched " + names[argmin(diff)]
+    print outText, "w/ dist", str(min(diff))
+    
+def compHash(thisHash, hash):
     diff = []
     for i in range(len(hash)):
         d = 0
@@ -57,31 +78,20 @@ def compareMe(num):
                 d += 1
         diff.append(d)
 
-    print diff
-    n = char[num].replace("\"","")
-    outText = "Input test-" + n +".png matched " + names[argmin(diff)]
-    if (n == names[argmin(diff)][-5]):
-        print outText, "correctly"
-        corr += 1
-    else:
-        print outText, "falsely"
-        fals += 1
-        print "dHash Hamming distance", str(min(diff))
-    #Img1 = Image.open("D:\\aai\\apa\\output\\" + names[num])
-    #Img1.show()
+    return diff
 
 def makeImage(num):
+    global sect, inc
+    
     b = []
     r = []
-    global sect, inc
     if sect[num].find("-") != -1:
         sect1 = sect[num][:sect[num].find("-")]
         sect2 = sect[num][sect[num].find("-")+1:]
     else:
         sect1 = sect[num]
         sect2 = -1
-    f = open("D:\\aai\\apa\\data\\" + inc + ".dat")
-    a = f.read()
+    a = open("D:\\aai\\apa\\data\\" + inc + ".dat").read()
     pe = re.findall("\.PEN_DOWN([.*\d*\n*\s*]*)\.PEN_UP", a)
     asdf1 = re.findall("[\s*](\d{1,})[\s*](\d{1,})", pe[int(sect1)])
     for i in range(len(asdf1)):
@@ -98,12 +108,11 @@ def makeImage(num):
     minX = min([int(k[0]) for k in r])
     minY = min([int(k[1]) for k in r])
 
-    for i in range(maxX-minX + 1):
+    for i in range(max(maxX-minX + 1, 9)):
         a = []
-        for j in range(maxY-minY + 1):
+        for j in range(max(maxY-minY + 1, 9)):
             a.append([255,255,255])
         b.append(a)
-
     for i in range(len(r)):
         b[int(r[i][0]) - minX][int(r[i][1]) - minY] = [0,0,0]
     
@@ -131,5 +140,7 @@ for root, dirs, files in os.walk("D:\\aai\\apa\\apa\\"):
             for i in range(len(char)):
                 compareMe(i)
 
-print "correct vs. false" 
-print "    ", corr, " : ", fals
+imgCompare()
+
+print "\ncorrect vs. false" 
+print "  ", corr, "  :  ", fals
