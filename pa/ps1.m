654d5221c01c26157c105d3c8f9838cb802a9174
Mirco Kocher <mirco.kocher@gmx.ch>
2013-10-09 22:34:13 +0200

    -*-ml&dm-*- serie1 weka restaurant problem done

--- pa/ps1.m
+++ pa/ps1.m
@@ -2,11 +2,13 @@ clear all;
 close all;
 clc;
 format long;
-N = 10^6;
+N = 1.25*10^6; % better choose < 1.7*10^6
 t = 100;
 A = rand(N,2,t); % N points of (x,y) and t times
-C = A(:,1,:).*A(:,1,:) + A(:,2,:).*A(:,2,:); % x^2 + y^2
+C = A(:,1,:).^2 + A(:,2,:).^2; % x^2 + y^2
+clear A;
 myPi = reshape(sum((C <=1)), 1, t).*4/N;
+clear C;
 min(myPi)
 max(myPi)
 mean(myPi)
