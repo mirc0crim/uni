f13e34d4d42e61866fb5fc11d68d0b947c4e83b3
Mirco Kocher <mirco.kocher@gmx.ch>
2013-10-13 16:41:30 +0200

    -*-pa-*- ps2 done, created function, return argmin

--- pa/ps2.m
+++ pa/ps2.m
@@ -1,22 +1,20 @@
-clear all;
-close all;
-clc;
-format long;
+function a = ps2(p1,p2)
+% calculates if the point [x,y] converges to z1, z2 or z3
 
+p = [p1; p2];
 z1 = [1; 0];
 z2 = [-0.5; sqrt(3)/2];
 z3 = [-0.5; -sqrt(3)/2];
 
 g = @(x,y) [x^3 - 3*x*y^2 - 1; 3*x^2*y - y^3];
 j = @(x,y) [3*x^2 - 3*y^2, -6*x*y; 6*x*y, 3*x^2 - 3*y^2];
+dist = @(x,y) sqrt(abs(x-y)' * abs(x-y));
 
-p = [2;1];
 d = 1;
 while d>0.01
     oldP = p;
     p = p - j(p(1),p(2))\g(p(1),p(2));
-    d = sqrt(abs(p-oldP)' * abs(p-oldP));
+    d = dist(p,oldP);
 end
 
-d
-p
+[d, a] = min([dist(p,z1), dist(p,z2), dist(p,z3)]);
