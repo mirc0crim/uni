37b14921dbc72c9262efcd278a85e244975c5164
Mirco Kocher <mirco.kocher@gmx.ch>
2013-10-16 13:17:28 +0200

    -*-snlp-*- author array, tag rem, AZDict

--- snlp/src/helperMK.py
+++ snlp/src/helperMK.py
@@ -1,11 +1,43 @@
 # -*- coding: ascii -*-
+import re
+import string
 from collections import Counter
+
+di = re.DOTALL|re.IGNORECASE
 
 def readFile(fileName):
     f = open(fileName)
     s = f.read()
     f.close()
-    return s
+    return s.lower()
+
+def authorArray(text):
+    s1 = ""
+    s2 = ""
+    s3 = ""
+    s4 = ""
+    m = re.findall("<DOCNO>.*?PUBLIUS", text, di)
+    for i in range(len(m)):
+        if re.findall("<AUTHOR> Alexander Hamilton </", m[i], di):
+            s1 += m[i]
+        elif re.findall("<AUTHOR> James Madison </", m[i], di):
+            s2 += m[i]
+        elif re.findall("<AUTHOR> John Jay </", m[i], di):
+            s3 += m[i]
+        else:
+            s4 += m[i]
+    if len(s4) <> 0:
+        print s4
+    return [s1,s2,s3,s4]
+
+def extractText(text):
+    rep = ["DOCNO", "DOCID", "AUTHOR", "TITLE", "SOURCE"]
+    for i in range(len(rep)):
+        text = re.sub("<" + rep[i] + ">.*?</" + rep[i] + ">", "", text, 0, di)
+        text = re.sub("\n\n","\n", text)
+    text = re.sub("To the People of the State of New York:\n", "", text, 0, di)
+    text = re.sub("PUBLIUS\n", "", text, 0, di)
+    return text
 
 def getMostFrequentNWords(t, n):
     return Counter(t.split()).most_common(n)
@@ -15,3 +47,13 @@ def getNumberOfWordTypes(t):
 
 def getNumberOfToken(t):
     return len(t.split())
+
+def getAZDict(text):
+    d = {}
+    c = Counter(text)
+    for e in string.lowercase:
+        if c.get(e):
+            d[e] = c.get(e)
+        else:
+            d[e] = 0
+    return d
--- snlp/src/main.py
+++ snlp/src/main.py
@@ -9,15 +9,22 @@ print "Path to Train Set", fedTrainPath
 print "Path to Test Set", fedTestPath
 print "Path for Output", outputPath
 
-train = helperMK.readFile(fedTrainPath)
+# [[hamiltonDocs][madisonDocs][JayDocs]]
+train = helperMK.authorArray(helperMK.readFile(fedTrainPath))
 test = helperMK.readFile(fedTestPath)
-
-print "Train:"
-print helperMK.getNumberOfToken(train)
-print helperMK.getNumberOfWordTypes(train)
-print helperMK.getMostFrequentNWords(train,10)
+for i in range(3):
+    train[i] = helperMK.extractText(train[i])
+test = helperMK.extractText(test)
+text = ["Hamilton Train:", "Madison Train:", "Jay Train:"]
+for i in range(len(text)):
+    print text[i]
+    print helperMK.getNumberOfToken(train[i])
+    print helperMK.getNumberOfWordTypes(train[i])
+    print helperMK.getMostFrequentNWords(train[i],5)
+    print helperMK.getAZDict(train[i])
 
 print "Test:"
 print helperMK.getNumberOfToken(test)
 print helperMK.getNumberOfWordTypes(test)
-print helperMK.getMostFrequentNWords(test,10)
+print helperMK.getMostFrequentNWords(test,5)
+print helperMK.getAZDict(test)
