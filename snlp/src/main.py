b797fcbb61297509e1731330cb4af1d8284c62a5
Mirco Kocher <mirco.kocher@gmx.ch>
2013-10-16 14:36:40 +0200

    -*-snlp-*- relative character frequency

--- snlp/src/main.py
+++ snlp/src/main.py
@@ -16,23 +16,22 @@ print "Path for Output", outputPath
 train = helperMK.authorArray(helperMK.readFile(fedTrainPath))
 test = helperMK.readFile(fedTestPath)
 
-
-feature = [[[],[],[],[]], [[],[],[],[]], [[],[],[],[]]]
+feature = [[[],[],[]], [[],[],[]], [[],[],[]]]
 for i in range(len(names)):
     print names[i]
     n1 = 0.0
     n2 = 0.0
-    n3 = 0.0
     d = helperMK.getAZDict(string.lowercase)
+    chars = 0.0
     for j in range(len(train[i])):
         train[i][j] = helperMK.extractText(train[i][j])
         n1 += helperMK.getNumberOfToken(train[i][j])
         n2 += helperMK.getNumberOfWordTypes(train[i][j])
-        #n3 += helperMK.getMostFrequentNWords(train[i][j],5)
         currDict = helperMK.getAZDict(train[i][j])
         for e in string.lowercase:
             d[e] = d[e] + currDict[e]
+        chars += len(train[i][j])
     for e in string.lowercase:
-        d[e] = d[e] / float(len(train[i]))
-    feature[i] = [n1/len(train[i]),n2/len(train[i]),n3/len(train[i]), d.values()]
+        d[e] = d[e] / chars * 26
+    feature[i] = [n1/len(train[i]), n2/len(train[i]), d.values()]
     print feature[i]
