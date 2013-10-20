45d9f44eaa72566b4cdc11037da1a53f2f887c54
Mirco Kocher <mirco.kocher@gmx.ch>
2013-10-20 20:54:07 +0200

    -*-cv-*- center, radius uf chrome sphere

--- cv/a1/solntemplate.m
+++ cv/a1/solntemplate.m
@@ -2,7 +2,12 @@
 
 clear all;
 close all;
-
-
+clc;
 
 % Put your code here
+
+circle = imread('Images/chrome/chrome.mask.png');
+circle = circle(:,:,1);
+[circleRow circleCol] = find(circle == 255);
+center = [(max(circleCol) + min(circleCol))/2, (max(circleRow) + min(circleRow))/2]
+radius = (max(circleRow) - min(circleRow))/2
