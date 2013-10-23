361371158a5c29067b37f08271d8f9bcf4349503
Mirco Kocher <mirco.kocher@gmx.ch>
2013-10-23 12:11:57 +0200

    -*-snlp-*- punctuation as vector

--- snlp/src/main.py
+++ snlp/src/main.py
@@ -13,36 +13,43 @@ fPersPron = ["i","me","my","mine","we","us","our","ours"]
 print "Path to Train Set", fedTrainPath
 print "Path to Test Set", fedTestPath
 print "Path for Output", outputPath
+print
 
 # [[hamiltonDocs][madisonDocs][JayDocs]]
 train = helperMK.authorArray(helperMK.readFile(fedTrainPath))
 test = helperMK.readFile(fedTestPath)
 
-feature = [[[],[],[]], [[],[],[]], [[],[],[]]]
-for i in range(len(names)):
-    print names[i]
+feature = [[],[],[]]
+for author in range(len(names)):
+    print names[author]
     numToken = 0.0
     numTypes = 0.0
-    punct = 0.0
+    punct = [0,0,0,0,0,0]
+    psum = 0.0
     pron = 0.0
     dictCount = helperMK.getAZDict(string.lowercase)
     chars = 0.0
-    docSize = len(train[i])
-    for j in range(docSize):
-        train[i][j] = helperMK.extractText(train[i][j])
-        numToken += helperMK.getNumberOfToken(train[i][j])
-        numTypes += helperMK.getNumberOfWordTypes(train[i][j])
-        currDict = helperMK.getAZDict(train[i][j])
+    for currDoc in range(len(train[author])):
+        train[author][currDoc] = helperMK.extractText(train[author][currDoc])
+        numToken += helperMK.getNumberOfToken(train[author][currDoc])
+        numTypes += helperMK.getNumberOfWordTypes(train[author][currDoc])
+        currDict = helperMK.getAZDict(train[author][currDoc])
         for e in string.lowercase:
-            dictCount[e] = dictCount[e] + currDict[e]
-        punct += sum([train[i][j].count(x) for x in punctMarks]) #make 6 dim vector
-        pron += sum([train[i][j].count(x) for x in fPersPron])
+            dictCount[e] += currDict[e]
+        for k, e in enumerate([train[author][currDoc].count(x) for x in punctMarks]):
+            punct[k] += e
+        psum += sum([train[author][currDoc].count(x) for x in punctMarks])
+        pron += sum([train[author][currDoc].count(x) for x in fPersPron])
         chars += sum(currDict.values())
     for e in string.lowercase:
+        dictCount[e] -= 1
         dictCount[e] /= chars / 26
-    numToken /= docSize
-    numTypes /= docSize
-    punct /= docSize
-    pron /= docSize * numToken
-    feature[i] = [numToken, numTypes, dictCount.values(), punct, pron]
-    print feature[i]
+    charList = []
+    for e in sorted(dictCount):
+        charList.append(dictCount[e])
+    for currDoc in range(6):
+        punct[currDoc] /= psum
+    pron /= numToken
+    wordLen = chars / numToken
+    feature[author] = [numToken/numTypes, charList, punct, pron, wordLen]
+    print feature[author]
