f29bb0e5abbdcca3efb841347d85f01d27ef459c
Mirco Kocher <mirco.kocher@gmx.ch>
2013-11-04 11:49:43 +0100

    -*-cv-*- first code for second assignment

---
+++ cv/a2/a2.m
@@ -0,0 +1,20 @@
+function a2()
+    clear All;
+    close All;
+    clc;
+    
+    name = 'tsukuba';
+    left = imread(['Images/', name, '-l.tiff']);
+    right = imread(['Images/', name, '-r.tiff']);
+    imgDim = size(left);
+    subplot(1,2,1);
+    imshow(left);
+    subplot(1,2,2);
+    imshow(right);
+    [x, y] = ginput(2);
+    x = round(x);
+    y = round(y);
+    nonvalid = find(x > imgDim(2) | y > imgDim(1) | x < 0 | y < 0);
+    x(nonvalid) = NaN;
+    y(nonvalid) = NaN;
+end
