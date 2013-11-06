ca18b1dbadd41554ed50b1d1af639a65e714c5e3
Mirco Kocher <mirco.kocher@gmx.ch>
2013-11-06 20:31:30 +0100

    -*-pa-*- correctly sorted coordinates for plot

--- pa/ps5.m
+++ pa/ps5.m
@@ -21,10 +21,10 @@ xdist = repmat(coordinates(:,1),1,131) - repmat(coordinates(:,1)',131,1);
 ydist = repmat(coordinates(:,2),1,131) - repmat(coordinates(:,2)',131,1);
 distances = sqrt(xdist.^2 + ydist.^2);
 distances(find(eye(131))) = Inf;
+
 route = zeros(131,131);
-myroute = ones(262,2);
 c = 1;
-while c < 131
+while c < 132
     index = find(distances == min(min(distances)), 1);
     [y,x] = ind2sub([131,131], index);
     distances(y,x) = Inf;
@@ -35,21 +35,19 @@ while c < 131
     incomming = sum(testRoute,1);
     outgoing = sum(testRoute,2);
     if max(incomming + outgoing') > 2
-        %disp('degree exeeded');
+        % degree exeeded
         valid = false;
     else
         for n = 1:c
             if trace(testRoute^n) ~= 0
-                %disp('cycle detected');
+                % cycle detected
                 valid = false;
                 break;
             end
         end
     end
-    if valid
-        %disp(['shortest distance from ', num2str(x), ' to ', num2str(y)]);
-        myroute(2*x-1,:) = coordinates(x,:);
-        myroute(2*x,:) = coordinates(y,:);
+    if valid || c == 131
+        % shortest distance from x to y
         route(y,x) = 1;
         distances(:,x) = Inf;
         distances(y,:) = Inf;
@@ -57,11 +55,17 @@ while c < 131
         disp(c)
     end
 end
-myroute(261,:) = myroute(260,:);
-myroute(262,:) = myroute(1,:);
 
 subplot(1,2,1);
 scatter(coordinates(:,1),coordinates(:,2));
 subplot(1,2,2);
-
-plot(myroute(:,1),myroute(:,2));
+i = 1;
+j = 1;
+A = ones(132,2);
+while i < 132
+    A(i,:) = coordinates(j,:);
+    j = find(route(j,:));
+    i = i + 1;
+end
+A(i,:) = coordinates(1,:);
+plot(A(:,1),A(:,2));
