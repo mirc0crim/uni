665bdbca2635c125c5afeb6d0abc3b25bd64c587
Mirco Kocher <mirco.kocher@gmx.ch>
2013-11-18 18:00:53 +0100

    -*-cv-*- fundMat, epipole, epipolar (forgot to commit some times)

--- cv/a2/a2.m
+++ cv/a2/a2.m
@@ -2,21 +2,42 @@ function a2()
     clear All;
     close All;
     clc;
-    
+
     name = 'tsukuba';
-    left = imread(['Images/', name, '-l.tiff']);
-    right = imread(['Images/', name, '-r.tiff']);
-    imgDim = size(left);
-    subplot(1,2,1);
-    imshow(left);
-    subplot(1,2,2);
-    imshow(right);
-    [x, y] = ginput(2);
-    x = round(x);
-    y = round(y);
-    nonvalid = find(x > imgDim(2) | y > imgDim(1) | x < 0 | y < 0);
-    x(nonvalid) = NaN;
-    y(nonvalid) = NaN;
-    x
-    y
+    imLeft = imread(['Images/', name, '-l.tiff']);
+    imRight = imread(['Images/', name, '-r.tiff']);
+    %[left, right] = cpselect(im_left, im_right, 'Wait', true)
+    left = [174,172; 178,156; 194,142; 194,132; 80,256; 47,81; 244,60; 311,222];
+    right = [163,171; 168,155; 187,143; 186,132; 68,256; 41,81; 240,60; 303,221];
+    f = fundMat(left, right)
+
+    epipoleEpipolar(imLeft, imRight, f, 'left', 'right')
+    xlabel('(click to continue)')
+    waitforbuttonpress;
+    epipoleEpipolar(imRight, imLeft, f', 'right', 'left')
+end
+
+function epipoleEpipolar(im1, im2, f, dir1, dir2)
+    imDim = size(im1);
+    figure(1);
+    imshow(im1);
+    title(['Choose a point in this ', dir1, ' image']);
+    pnt = ginput(1);
+    hold on;
+    plot(pnt(1), pnt(2), 'rx');
+    hold off;
+    figure(2);
+    imshow(im2);
+    title(['Here is the epipole and epipolar line in the ', dir2,' image']);
+    hold on;
+    fp = f*[pnt, 1]';
+    x1 = -fp(3)/fp(1);
+    y2 = -fp(3)/fp(2);
+    plot([x1,0,x1-imDim(2)*fp(2)/fp(1),imDim(1)], [0,y2,imDim(2),y2-imDim(1)*fp(1)/fp(2)], 'r');
+    epipol = null(f);
+    epipol = epipol/epipol(3);
+    disp([dir2, ' epipol']);
+    disp(epipol(1:2));
+    plot(epipol(1), epipol(2), 'og');
+    hold off
 end
---
+++ cv/a2/fundMat.m
@@ -0,0 +1,24 @@
+function f = fundMat(left, right)
+
+    l = [left, ones(length(left), 1)]';
+    r = [right, ones(length(right), 1)]';
+    left_normed = normMat(l)*l;
+    right_normed = normMat(r)*r;
+    u2u1u2v1u2 = repmat(right_normed(1,:)', 1, 3).*left_normed';
+    v2u1v2v1v2 = repmat(right_normed(2,:)', 1, 3).*left_normed';
+    eightPntMat = [u2u1u2v1u2, v2u1v2v1v2, left_normed'];
+    [~, ~, V] = svd(eightPntMat);
+    [fundU, fundS, fundV] = svd(reshape(V(:,9), 3, 3)');
+    f = normMat(r)' * fundU*diag([fundS(1,1), fundS(2,2), 0])*fundV' * normMat(l);
+end
+
+function m = normMat(vect3N)
+    % Normalize a vector of 3xN coordinates by subtracting centroid
+    c = mean(vect3N, 2);
+    means = repmat(c, 1, length(vect3N));
+    dist = sqrt(sum((vect3N - means).^2));
+    mean_dist= mean(dist);
+    m = [2/mean_dist, 0, -2/mean_dist*c(1);...
+        0, 2/mean_dist, -2/mean_dist*c(2);...
+        0, 0, 1];
+end
