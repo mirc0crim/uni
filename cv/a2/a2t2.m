c5729fef9a2a4f9999c17fd0b99e364a9ee0becf
Mirco Kocher <mirco.kocher@gmx.ch>
2013-11-18 21:41:36 +0100

    -*-cv-*- subset-removed entries randomly

--- cv/a2/a2t2.m
+++ cv/a2/a2t2.m
@@ -3,8 +3,19 @@ close All;
 clc;
 
 file = fopen('data/Matched point set.txt','r');
-%matched = fscanf(file, '%d %d %d %d', [4, inf]);
 C = textscan(file, '%d %d %d %d');
+fclose(file);
 left = [C{1},C{2}];
 right = [C{3},C{4}];
-fclose(file);
+%remove random entries to keep only a subset of 20%
+deletions = rand(length(left),1);
+deletions(deletions >= 0.8) = 1;
+left = double(left(deletions==1,:));
+right = double(right(deletions==1,:));
+
+f = fundMat(left, right);
+disp(['Funamental Matrix has rank ', int2str(rank(f))]);
+disp(f);
+
+%from the intrinsic parameter file
+K = [-83.33333, 0.0, 250.0; 0.0, -83.33333, 250.0; 0.0, 0.0, 1.0];
