bb4ffbf109ae817151eeec52c912057a25e156fb
Mirco Kocher <mirco.kocher@gmx.ch>
2013-11-19 12:03:05 +0100

    -*-cv-*- rotation and translation, 3d reconstruct

--- cv/a2/a2t2.m
+++ cv/a2/a2t2.m
@@ -20,3 +20,19 @@ disp(fundamentalMatrix);
 %from the intrinsic parameter file
 K = [-83.33333, 0.0, 250.0; 0.0, -83.33333, 250.0; 0.0, 0.0, 1.0];
 EssentialMatrix = K'*fundamentalMatrix*K
+[U, D, V] = svd(EssentialMatrix);
+W = [0,-1,0; 1,0,0; 0,0,1];
+tx = V*W*D*V';
+R = U*W'*V';
+t = [tx(6),tx(7),tx(2)]'
+
+x3 = zeros(1, length(left));
+for i=1:length(left)
+    r1 = R(1,:);
+    y1r3 = right(i,1)*R(3,:);
+    x3(i) = dot((r1-y1r3),t) / dot((r1-y1r3),[left(i,:),1]);
+end
+left3d = [left'; x3];
+left3d(1,:) = left3d(1,:).*left3d(3,:);
+left3d(2,:) = left3d(2,:).*left3d(3,:);
+scatter3(left3d(1,:), left3d(2,:), left3d(3,:), 'x')
