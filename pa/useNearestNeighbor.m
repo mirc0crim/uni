40ccd6363fb603ec2907a2a8d34fe30e20c9974a
Mirco Kocher <mirco.kocher@gmx.ch>
2013-12-21 18:06:27 +0100

    -*-pa-*- nearest neighbor construction heuristics

---
+++ pa/useNearestNeighbor.m
@@ -0,0 +1,20 @@
+function [route, dist, startCity] = useNearestNeighbor(distances)
+    origDistances = distances;
+    noOfCities = length(distances);
+    route = zeros(noOfCities,noOfCities);
+    startCity = ceil(rand(1)*411);
+    cityA = startCity;
+    distances(:,startCity) = inf;
+    c = 1;
+    dist = 0;
+    while c < noOfCities
+        [~, cityB] = min(distances(cityA,:));
+        route(cityA, cityB) = 1;
+        dist = dist + distances(cityA, cityB);
+        distances(:,cityB) = inf;
+        cityA = cityB;
+        c = c + 1;
+    end
+    route(cityA, startCity) = 1;
+    dist = dist + origDistances(cityA, startCity);
+end
